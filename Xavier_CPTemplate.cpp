54c88cfc44c19efde284a03e2fc88cb789f1d112
xavierbeast68 <sdby101@gmail.com>
2022-12-26 23:33:43 +0530

    template update

--- Xavier_CPTemplate.cpp
+++ Xavier_CPTemplate.cpp
@@ -13,7 +13,12 @@
 
 
 #include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
 #define endl                            '\n'
 #define cendl                           cout<<endl
 #define int                             long long
